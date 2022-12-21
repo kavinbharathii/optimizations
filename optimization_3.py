5ea6d5ba1314fb8225f5d24067d66323a02dbc40
Kavin Bharathi <66783110+kavinbharathii@users.noreply.github.com>
2022-12-22 00:12:45 +0530

    Add files via upload

---
+++ optimization_3.py
@@ -0,0 +1,20 @@
+from collections import Counter
+from optimus import optimus_compare as opticmp
+
+def unoptimized(my_list) :
+    new_list = []
+    for i in set(my_list) :
+        Count = my_list.count(i)
+        new_list.append([i,Count])
+
+    return sorted(new_list, key = lambda x: -x[1])
+
+def optimized(my_list) :
+    counter = Counter(my_list)
+    return counter
+
+s = [100, 23, 55, 103, 90, 68, 1]
+
+print()
+opticmp('unoptimized', 'optimized', (s, ), (s, ), verbose = True)
+print()
---
+++ optimization_4.py
@@ -0,0 +1,15 @@
+import sys
+from optimus import optimus_compare as opticmp
+
+def unoptimized(iterations) :
+    user_data = [ i*i for i in range(iterations)]
+    return sys.getsizeof(user_data) , 'bytes'
+
+
+def optimized(iterations) :
+    user_data = (i*i for i in range(iterations))
+    return sys.getsizeof(user_data) , 'bytes'
+
+print()
+opticmp('unoptimized', 'optimized', (10_000, ), (10_000, ), verbose = True, space_opt = True)
+print()
